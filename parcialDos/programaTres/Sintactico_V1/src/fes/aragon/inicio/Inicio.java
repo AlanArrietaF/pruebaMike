f427ad78029b31185711dad52fe902b4df385d05
·AlanArrietaF· <arrietafloresalanalejandro@gmail.com>
2026-05-06 16:53:21 -0600

    Finalizacion del tercer programa

--- parcialDos/programaTres/Sintactico_V1/src/fes/aragon/inicio/Inicio.java
+++ parcialDos/programaTres/Sintactico_V1/src/fes/aragon/inicio/Inicio.java
@@ -3,7 +3,6 @@ package fes.aragon.inicio;
 import fes.aragon.codigo.Lexico;
 import fes.aragon.codigo.Sym;
 import fes.aragon.codigo.Tokens;
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -18,80 +17,94 @@ public class Inicio {
         BufferedReader buf;
         try {
             buf = new BufferedReader(
-                    new FileReader(System.getProperty("user.dir")
-                            + "/archivo.txt"));
+                    new FileReader(System.getProperty("user.dir") + "/archivo.txt"));
             ap.analizador = new Lexico(buf);
+
+            // Según la imagen 1: Función secuencia() inicia con un get_token();
             ap.siguienteToken();
-            ap.inicioAnalisis();
+            ap.secuencia();
 
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private void inicioAnalisis() {
-        while (tokens.getLexema() != Sym.EOF) {
-            // Saltamos líneas vacías
-            if (tokens.getLexema() == Sym.SALTOLINEA) {
-                siguienteToken();
-                continue;
+    /**
+     * Implementación de la Imagen 1: Función secuencia()
+     */
+    private void secuencia() {
+        do {
+            expresion(); // Llamada a E()
+
+            // Bucle interno: while (token != PUNTOYCOMA) { Error; get_token(); }
+            while (tokens.getLexema() != Sym.PUNTOCOMA && tokens.getLexema() != Sym.EOF) {
+                System.out.println("Error en expresión en línea: " + (tokens.getLinea() + 1));
+                this.error = false;
+                siguienteToken(); // get_token() del pseudocódigo
             }
 
-            E(); // Llamada a la regla inicial de la gramática
-
-            // Verificamos el fin de instrucción (Punto y Coma)
-            if (tokens.getLexema() != Sym.PUNTOCOMA) {
-                errorSintactico();
-            }
-
-            if (!this.error) {
-                System.out.println("Invalida linea= " + (tokens.getLinea() + 1));
-                this.error = true;
+            if (this.error) {
+                System.out.println("Valida línea: " + (tokens.getLinea() + 1));
             } else {
-                System.out.println("Valida  linea= " + (tokens.getLinea() + 1));
+                System.out.println("Invalida línea: " + (tokens.getLinea() + 1));
+                this.error = true; // Reset para la siguiente línea
             }
 
-            siguienteToken();
-        }
-    }
-
-    // E ::= T or E | T
-    private void E() {
-        T();
-        if (tokens.getLexema() == Sym.OR) {
-            siguienteToken();
-            E();
-        }
-    }
-
-    // T ::= F and T | F
-    private void T() {
-        F();
-        if (tokens.getLexema() == Sym.AND) {
-            siguienteToken();
-            T();
-        }
-    }
-
-    // F ::= not F | true | false | ( E )
-    private void F() {
-        if (tokens.getLexema() == Sym.NOT) {
-            siguienteToken();
-            F();
-        } else if (tokens.getLexema() == Sym.TRUE) {
-            siguienteToken();
-        } else if (tokens.getLexema() == Sym.FALSE) {
-            siguienteToken();
-        } else if (tokens.getLexema() == Sym.PAR_A) {
-            siguienteToken();
-            E();
-            if (tokens.getLexema() == Sym.PAR_C) {
+            // get_token() después del while para consumir el PUNTOYCOMA
+            if (tokens.getLexema() == Sym.PUNTOCOMA) {
                 siguienteToken();
-            } else {
-                this.error = false; // Falta paréntesis de cierre
             }
-        } else {
-            this.error = false; // Token no reconocido por la gramática F
+
+        } while (tokens.getLexema() != Sym.EOF);
+    }
+
+    // E ::= T { or T }
+    private void expresion() {
+        termino();
+        while (tokens.getLexema() == Sym.OR) {
+            siguienteToken();
+            termino();
+        }
+    }
+
+    // T ::= F { and F }
+    private void termino() {
+        factor();
+        while (tokens.getLexema() == Sym.AND) {
+            siguienteToken();
+            factor();
+        }
+    }
+
+    /**
+     * Implementación de la Imagen 2: Función factor()
+     */
+    private void factor() {
+        int lex = tokens.getLexema();
+
+        // switch (token) del pseudocódigo
+        if (lex == Sym.TRUE || lex == Sym.FALSE) { // Representan NUM o ID en tu lógica
+            siguienteToken();
+        }
+        else if (lex == Sym.NOT) {
+            siguienteToken();
+            factor(); // Llamada recursiva a factor() según diagrama
+        }
+        else if (lex == Sym.PAR_A) { // AB_PARID en tu pseudocódigo
+            siguienteToken();
+            expresion();
+
+            if (tokens.getLexema() != Sym.PAR_C) { // CE_PAR en tu pseudocódigo
+                System.out.println("Error: Paréntesis de cierre faltante");
+                this.error = false;
+            } else {
+                siguienteToken();
+            }
+        }
+        else {
+            // default: Error: Expresión no válida
+            System.out.println("Error: Expresión no válida en columna " + tokens.getColumna());
+            this.error = false;
         }
     }
 
@@ -101,21 +114,10 @@ public class Inicio {
             if (tokens == null) {
                 tokens = new Tokens("EOF", Sym.EOF, 0, 0);
             } else {
-                System.out.println("Token: " + Sym.terminales[tokens.getLexema()] +
-                        " | Lexema: " + tokens.getToken() +
-                        " | L: " + tokens.getLinea() +
-                        " | C: " + tokens.getColumna());
+                System.out.println("Token detectado: " + Sym.terminales[tokens.getLexema()]);
             }
         } catch (IOException ex) {
             tokens = new Tokens("EOF", Sym.EOF, 0, 0);
-        }
-    }
-
-    private void errorSintactico() {
-        this.error = false;
-        // Recuperación: consumir hasta el delimitador o fin de archivo
-        while (tokens.getLexema() != Sym.PUNTOCOMA && tokens.getLexema() != Sym.EOF) {
-            siguienteToken();
         }
     }
 }
